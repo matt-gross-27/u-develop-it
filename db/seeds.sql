9a08fe630fc4ef5b9e9a55e61a4bf21718fec9ba
Matt Gross <mbgross111@gmail.com>
2021-03-03 14:52:45 -0800

    created election.db and candidates table with schema and seed files

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@ INSERT INTO candidates (first_name, last_name, industry_connected)
 VALUES
   ('Ronald', 'Firbank', 1),
   ('Virginia', 'Woolf', 1),
-  ('Piers', 'Gaveston', 1),
+  ('Piers', 'Gaveston', 0),
   ('Charles', 'LeRoi', 1),
   ('Katherine', 'Mansfield', 1),
   ('Dora', 'Carrington', 0),
