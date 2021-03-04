a3f1fb6ffb982be2bbbbb3b258e53a6a7a185f0f
Matt Gross <mbgross111@gmail.com>
2021-03-04 15:56:50 -0800

    add update candidate put route

--- server.js
+++ server.js
@@ -110,6 +110,80 @@ app.post('/api/candidates', ({ body }, res) => {
   });
 });
 
+// UPDATE candidate by id
+app.put('/api/candidates/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `
+    UPDATE candidates 
+    SET party_id = ?
+    WHERE id = ?
+  `;
+  const params = [req.body.party_id, req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: req.body,
+      changes: this.changes
+    });
+  });
+});
+
+// GET all parties
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// GET party by id
+app.get('/api/parties/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`
+  params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// DELETE party by id
+app.delete('/api/parties/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+    res.json({
+      message: 'successfully deleted',
+      changes: this.changes
+    });
+  });
+});
+
 // LAST ROUTE - Default response for any request Not Found (Catch All)
 app.use((req, res) => {
   res.status(404).end();
